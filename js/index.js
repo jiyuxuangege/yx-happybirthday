b380a9b03454140660a06108e6a7d8c34db0a76c
xingyuefeng <xyf@nihaosi.com>
2019-02-10 04:45:33 +0800

    ifrst commit

---
+++ js/index.js
@@ -0,0 +1,28 @@
+$(function() {
+  var benediction = "祝 杨 三 金 十 八 周 岁 六 周 年 生 日 快 乐 ! 俗 话 说: 年 初 六 送 穷 鬼,  迎 寿 星。".split(' ');
+  var benediction2 = `
+  新 的 一 年 没 话 说, 钱 包 钱 塞 不 下, 知 识 一 学 就 会, 医 术 小 华 佗。
+  然 后 最 重 要 的 是 开 开 心 心 下 去 , 我 觉 得 你 的 笑 容 超 灿 烂 哈。
+  再 然 后 祝 福 的 话 想 不 到 了,  日 子 长 慢 慢 补 充 😎。`.split(' ');
+
+  function print(text, dom, time) {
+    var timer = null;
+    var timeIdx= 0;
+    var countIdx =  text.length;
+    timer = setInterval(function() {
+
+      dom.text(dom.text() + text[timeIdx])
+
+      timeIdx++;
+      if (timeIdx === countIdx) {
+        clearInterval(timer)
+      }
+    }, time)
+  
+  }
+
+  var benedDom = $("#benediction");
+  var benedDom2 = $("#benediction2");
+  print(benediction, benedDom, 100)
+  print(benediction2, benedDom2, 300)
+})
